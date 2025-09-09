f5628a92eb627b6bb76eebcf2fa22952eadeb96c
David Andrew <david.andrew@kainos.com>
2025-09-09 08:30:46 +0100

    Initial Commit

---
+++ src/main/java/org/dwa/User.java
@@ -0,0 +1,37 @@
+package org.dwa;
+
+public class User {
+
+    private String Username;
+    private String Password;
+
+    public User(){
+
+    }
+
+    public User(String username, String password){
+        setUsername(username);
+        setPassword(password);
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+
+
+
+
+}
