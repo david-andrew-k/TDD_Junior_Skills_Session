6cef7671eb25d0051c5c2dd9e7a6da406ab0c2c8
David Andrew <david.andrew@kainos.com>
2025-09-11 13:55:09 +0100

    Added Username Tests

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,4 +1,5 @@
 import org.dwa.InvalidPasswordException;
+import org.dwa.InvalidUsernameException;
 import org.dwa.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,6 +17,42 @@ public class UserTest {
         String actual = testUserObject.toString();
         assertEquals(expected,actual);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"davidandrew", "user01", "AliceInWonderlandUsr", "JOHN1", "123", "PETER"})
+    void TestValidUsernameSuccessfullySetsUsername(String validUsername) {
+        User testUserObject = new User();
+        testUserObject.setUsername(validUsername);
+        String actual = testUserObject.getUsername();
+        assertEquals(validUsername, actual);
+    }
+
+    @Test
+    void TestInvalidUsernameTooLongThrowsInvalidUsernameException(){
+        User testUserObject = new User();
+        Exception actual = assertThrows(InvalidUsernameException.class, () -> testUserObject.setUsername("a".repeat(21)));
+        String expectedMessage = "Username must be no more than 20 characters long.";
+        assertEquals(expectedMessage, actual.getMessage());
+    }
+
+    @Test
+    void TestInvalidUsernameTooShortThrowsInvalidUsernameException(){
+        User testUserObject = new User();
+        Exception actual = assertThrows(InvalidUsernameException.class, () -> testUserObject.setUsername("aa"));
+        String expectedMessage = "Username must be at least 3 characters long.";
+        assertEquals(expectedMessage, actual.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Abc.", "User_01", "Alice-In-Wonderland", "JOHN!", "user name", "user@name"})
+    void TestInvalidUsernameInvalidCharactersThrowsInvalidUsernameException(){
+        User testUserObject = new User();
+        Exception actual = assertThrows(InvalidUsernameException.class, () -> testUserObject.setUsername("Abc."));
+        String expectedMessage = "Username can only contain letters and numbers.";
+        assertEquals(expectedMessage, actual.getMessage());
+    }
+
+
 
     @ParameterizedTest
     @ValueSource(strings = {"Password123@", "4apPlE$", "My13Password%&345678", "pA1#4"})
